aefccf37cbc625ef80c2480fdfe8d5e2d5efa478
quoctm17 <tranminhquoc0711@gmail.com>
2024-06-27 14:55:17 +0700

    first commit

---
+++ BadmintonRentalPlatformClient/Pages/BadmintonCourt/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BadmintonRentalPlatformClient.Pages.BadmintonCourt
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
