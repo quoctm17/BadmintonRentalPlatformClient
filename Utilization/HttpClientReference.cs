ccdbf54d439bc050bc5e191f660ad692e85a0cea
alannguyen2003 <nguyenho30112003@gmail.com>
2024-07-18 23:35:06 +0700

    add http client reference for calling api and project for admin template

---
+++ Utilization/HttpClientReference.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http.Json;
+
+namespace Utilization;
+
+public static class HttpClientReference
+{
+    public static T GetHttpClient<T>(string url) where T : class
+    {
+        var httpClient = new HttpClient();
+        var result = httpClient.GetFromJsonAsync<T>(url);
+        if (result != null) return result.Result;
+        return null;
+    }
+}
