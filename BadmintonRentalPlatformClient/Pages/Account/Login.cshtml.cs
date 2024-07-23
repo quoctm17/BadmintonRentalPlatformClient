24ce3efa35cc6120de068da05454fc4a5f27bc43
alannguyen2003 <nguyenho30112003@gmail.com>
2024-07-23 11:08:26 +0700

    add login for user

--- BadmintonRentalPlatformClient/Pages/Account/Login.cshtml.cs
+++ BadmintonRentalPlatformClient/Pages/Account/Login.cshtml.cs
@@ -1,12 +1,41 @@
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using BadmintonRentalPlatformClient.Configuration;
+using DTOs;
+using DTOs.Response.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Utilization;
 
 namespace BadmintonRentalPlatformClient.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public string Email { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
         {
+            var client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:7020/");
+            var json = JsonSerializer.Serialize(new
+            {
+                email = Email,
+                password = Password
+            });
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = client.PostAsync("api/v1/login", content).Result;
+            var responseContent = response.Content.ReadAsStringAsync().Result;
+            var result = JsonSerializer.Deserialize<Result<LoginResponse>>(responseContent);
+            if (result.data != null) 
+            {
+                SessionHelper.SetObjectAsJson(HttpContext.Session, "username", result.data.FullName);
+                return RedirectToPage("../Index");
+            }
+            return RedirectToPage("./Login");
         }
     }
 }
