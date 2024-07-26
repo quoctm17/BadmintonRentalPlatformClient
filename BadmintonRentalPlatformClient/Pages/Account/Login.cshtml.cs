0c65741c833b458ed4fde7185dc52af6bfb81890
quoctm17 <tranminhquoc0711@gmail.com>
2024-07-26 09:08:17 +0700

    Fix Login, Change Languague to Vietnamese

--- BadmintonRentalPlatformClient/Pages/Account/Login.cshtml.cs
+++ BadmintonRentalPlatformClient/Pages/Account/Login.cshtml.cs
@@ -16,26 +16,6 @@ namespace BadmintonRentalPlatformClient.Pages.Account
         public string Email { get; set; }
         [BindProperty]
         public string Password { get; set; }
-
-        public async Task<IActionResult> OnPostAsync()
-        {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:7020/");
-            var json = JsonSerializer.Serialize(new
-            {
-                email = Email,
-                password = Password
-            });
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-            var response = client.PostAsync("api/v1/login", content).Result;
-            var responseContent = response.Content.ReadAsStringAsync().Result;
-            var result = JsonSerializer.Deserialize<Result<LoginResponse>>(responseContent);
-            if (result.data != null) 
-            {
-                SessionHelper.SetObjectAsJson(HttpContext.Session, "username", result.data.FullName);
-                return RedirectToPage("../Index");
-            }
-            return RedirectToPage("./Login");
-        }
+        
     }
 }
